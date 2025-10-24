0e6bca45c62b8cc2f2c2d993c5b08fec53a5d7d6
Theo Fabiano <theo.fabianomattei@gmail.com>
2025-10-24 00:26:42 -0400

    feat: tRPC Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TRPCReactProvider } from "@/trpc/client";
+import { ReactElement } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,14 +22,14 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
-}>) {
+  children: ReactElement;
+}>): ReactElement {
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
   );
