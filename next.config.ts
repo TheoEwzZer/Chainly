f3513455ae1213e4cc0454bb2059f859b356ec99
Theo Fabiano <theo.fabianomattei@gmail.com>
2025-11-25 01:33:16 -0500

    feat: enhance security by adding Content Security Policy and X-XSS-Protection headers in Next.js and Vercel configurations

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,28 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
+const isDev: boolean = process.env.NODE_ENV === "development";
+
+const ContentSecurityPolicy: string = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data: blob: https:;
+  font-src 'self';
+  connect-src 'self' 
+    https://*.ingest.sentry.io 
+    https://*.ingest.de.sentry.io 
+    https://*.ingest.us.sentry.io
+    wss://*.inngest.com
+    ${isDev ? " ws://localhost:* http://localhost:*" : ""};
+  frame-ancestors 'none';
+  base-uri 'self';
+  form-action 'self';
+  ${isDev ? "" : "upgrade-insecure-requests;"}
+`
+  .replaceAll(/\s{2,}/g, " ")
+  .trim();
+
 const securityHeaders = [
   {
     key: "X-DNS-Prefetch-Control",
@@ -25,6 +47,14 @@ const securityHeaders = [
   {
     key: "Permissions-Policy",
     value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy,
   },
 ];
 
